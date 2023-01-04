f25935a87a89b0eab505fc10185de6f54076c8d1
Amarnath <113051198+AmarnathBheema@users.noreply.github.com>
2023-01-04 17:27:19 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/AmarnathBheema/python-app.git
-cd python-app
+cd python-app/python
 sudo yum -y install python3-pip
 pip3 install -r requirements.txt
 python3 app.py
